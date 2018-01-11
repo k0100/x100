1cb84b826a995b8f4e2b4dddea167e9591512581
Mitko Kostov <kostov.email@gmail.com>
2018-01-11 22:25:29 +0200

    Removal of Clock Widget

--- client/app/core/widgets/widget-description/widget-descriptor.service.ts
+++ client/app/core/widgets/widget-description/widget-descriptor.service.ts
@@ -7,7 +7,6 @@ import { ServerCommand } from '../../../core/server/command'
 import { Identity } from '../../../core/server/identity'
 import { TodoComponent } from '../../../widgets/todo/todo.component';
 import { NoteComponent } from '../../../widgets/note/note.component';
-import { ClockComponent } from '../../../widgets/clock/clock.component';
 import { HttpService1 } from "../../http/http-intercept";
 
 
